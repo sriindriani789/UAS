7c7aa074ecf097037361900d03549ca316ff3f70
sriindriani789 <115676161+sriindriani789@users.noreply.github.com>
2023-01-08 19:14:20 +0800

    "UAS PEMROGRAMAN BERKERANGKA KERJA"

---
+++ Ngilmu.web/index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RUANG BELAJAR</title>
+    <link rel="stylesheet" href="s4.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <!-- navbar -->
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Ngil<span>mu</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#teams" class="menu-btn">Tutor</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+                <li><a href="index2.php">Login</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+    <!-- home -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">ALOHAAA</div>
+                <div class="text-2">Ngilmu</div>
+                <div class="text-3"><span class="typing"></span></div>
+                
+            </div>
+        </div>
+    </section>
+    <!-- akhir home -->
+
+    <!-- about -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/sr.png" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Ngilmu<span class="typing-2"></span></div>
+                    <p>Ngilmu adalah suatu komunitas belajar yang didirikan oleh mahasiswa Pendidikan Teknologi Informasi Angkatan 2020 dengan tujuan Untuk menunjang perkuliahan di bidang Teknologi Informasi dan  dimana anggotanya berpartisipasi dalam melakukan kegiatan belajar serta berinteraksi untuk mewujudkan tujuan belajar bersama. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- akhir about -->
+
+    <!-- services -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Multimedia</div>
+                        <p>Komunitas Multimedia Pendidikan Teknologi Informasi</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">RPL</div>
+                        <p>Komunitas Rekayasa Perangkat Lunak Pendidikan Teknologi Informas</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Pemrograman</div>
+                        <p>Komunitas Pemrograman Pendidikan Teknologi Informas</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+    <!-- akhir services -->
+
+
+    <!-- teams -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">Tutor</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/tutor1.png" alt="">
+                        <div class="text">Putri Aulia</div>
+                        
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/tutor2.png" alt="">
+                        <div class="text">Sabda Putra</div>
+                        
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/tutor3.png" alt="">
+                        <div class="text">Karlina Selviana</div>
+                        
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/tutor4.png" alt="">
+                        <div class="text">Fanny Rofalina</div>
+                       
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/tutor5.png" alt="">
+                        <div class="text">Prasdiyanto</div>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- akhir team -->
+
+
+   
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Nama</div>
+                                <div class="sub-title">Sri Indriani</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Alamat</div>
+                                <div class="sub-title">Lombok, West Nusa Tenggara</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">sriindriani977@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="image">
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="">SriIndriani</a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
